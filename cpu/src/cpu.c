ca8f64c6f8df42840620a28e09e48f9666b7252c
maykel <maykelcd83@gmail.com>
2024-08-31 00:44:38 +0000

    primeros cambios a cpu y memoria

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -2,15 +2,40 @@
 #include <cpu.h>
 pthread_t hilo_CPU_Cliente
 int main(int argc, char* argv[]) {
-    //Inicializar cpu
+
    
-    inicializar_cpu();
 
-    //Conectarnos como cliente a memoria
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
     
-    fd_memoria=crear_conexion(IP_MEMORIA,PUERTO_MEMORIA);
-    log_info(cpu_logger,"Conexion memoria exitosa");
+    IP_MEMORIA=config_get_string_value(cpu_config,"IP_MEMORIA");
+    PUERTO_MEMORIA=config_get_string_value(cpu_config,"PUERTO_MEMORIA")
+    PUERTO_ESCUCHA_DISPATCH=config_get_string_value(cpu_config,"PUERTO_ESCUCHA_DISPATCH");
+    PUERTO_ESCUCHA_INTERRUPT=config_get_string_value(cpu_config,"PUERTO_ESCUCHA_INTERRUPT");
 
-    //saludar("cpu");
+    fd_cpu_dispatch=iniciar_servidor(PUERTO_ESCUCHA_DISPATCH,cpu_logger,"Servidor CPU Dispatch iniciado");
+    fd_cpu_interrupt=iniciar_servidor(PUERTO_ESCUCHA_INTERRUPT,cpu_logger,"Servidor CPU Interrupt iniciado");
+    
+    
+    fd_memoria=crear_conexion(IP_MEMORIA,PUERTO_MEMORIA,"Conexion con MEMORIA exitosa");
+    
+    
     return 0;
 }
