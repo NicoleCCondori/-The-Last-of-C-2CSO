c47635de86e0c57aaa8a5910b1553b889754a4c0
NicoleCCondori <ncondoricordova@frba.utn.edu.ar>
2024-09-07 23:01:23 +0000

    idea para modularizar kernel

--- kernel/src/kernel.h
+++ kernel/src/kernel.h
@@ -1,44 +1,10 @@
 #ifndef KERNEL_H_
 #define KERNEL_H_
 
-#include <utils/utils.h>
 
-typedef struct{
-	t_config* config;
-	char* ip_memoria;
-	char* puerto_escucha;
-	char* puerto_memoria;
-	char* ip_cpu;
-	char* puerto_cpu_dispatch;
-	char* puerto_cpu_interrupt;
-	char* algoritmo_planificacion;
-	char* quantum;
-	char* log_level;
-}t_config_kernel;
+#include <kernel_utils.h>
 
-t_log* kernel_logger;
-t_log* kernel_logs_obligatorios;
 
-t_config_kernel* valores_config_kernel;
-
-int fd_cpu_dispatch;
-int fd_cpu_interrupt;
-int fd_memoria;
-
-pthread_t hilo_cpu_dispatch;
-pthread_t hilo_cpu_interrupt;
-pthread_t hilo_memoria;
-
-void inicializar_kernel();
-void configurar_kernel();
-
-void conectar_cpu_dispatch();
-void conectar_cpu_interrupt();
-void conectar_memoria();
-
-void kernel_escucha_cpu_dispatch();
-void kernel_escucha_cpu_interrupt();
-void kernel_escucha_memoria();
 
 
 #endif
---
+++ kernel/src/kernel_utils.h
@@ -0,0 +1,45 @@
+#ifndef KERNEL_UTILS_H_
+#define KERNEL_UTILS_H_
+
+#include <utils/utils.h>
+//#include <kernel_gestor.h>
+
+typedef struct{
+	t_config* config;
+	char* ip_memoria;
+	char* puerto_escucha;
+	char* puerto_memoria;
+	char* ip_cpu;
+	char* puerto_cpu_dispatch;
+	char* puerto_cpu_interrupt;
+	char* algoritmo_planificacion;
+	char* quantum;
+	char* log_level;
+}t_config_kernel;
+
+//Variables globales
+extern t_log* kernel_logger;
+extern t_log* kernel_logs_obligatorios;
+
+extern t_config_kernel* valores_config_kernel;
+
+extern int fd_cpu_dispatch;
+extern int fd_cpu_interrupt;
+extern int fd_memoria;
+
+extern pthread_t hilo_cpu_dispatch;
+extern pthread_t hilo_cpu_interrupt;
+extern pthread_t hilo_memoria;
+
+void inicializar_kernel();
+void configurar_kernel();
+
+void conectar_cpu_dispatch();
+void conectar_cpu_interrupt();
+void conectar_memoria();
+
+void kernel_escucha_cpu_dispatch();
+void kernel_escucha_cpu_interrupt();
+void kernel_escucha_memoria();
+
+#endif
