8c42a8a6ffc2a65da1e6ed66ac1d98bd39f0f39c
maykel <maykelcd83@gmail.com>
2024-09-13 14:38:20 +0000

    modularizo cpu

---
+++ cpu/src/escuchar_kernel_interrupt.c
@@ -0,0 +1,28 @@
+#include <escuchar_kernel_interrupt.h>
+#include <c_conexiones.h>
+
+
+
+
+void escuchar_kernel_interrupt(){
+    //atender los msjs de kernel-interrupt 
+    bool control_key=1;
+    while (control_key)
+	{
+		int cod_op = recibir_operacion(fd_kernel_interrupt);
+		switch (cod_op)
+		{
+		case MENSAJE:
+
+		case PAQUETE:
+
+			break;
+		case -1:
+			log_error(cpu_logger, "Desconexion de KERNEL-Interrupt");
+			exit(EXIT_FAILURE);
+		default:
+			log_warning(cpu_logger, "Operacion desconocida de KERNEL-Interrupt");
+			break;
+		}
+	}
+}
