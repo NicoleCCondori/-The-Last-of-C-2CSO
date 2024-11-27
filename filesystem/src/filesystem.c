83f2b7d80db269e7209fab0a689279157c00584b
maykel <maykelcd83@gmail.com>
2024-11-27 13:20:42 +0000

    pasandome lo de filesystem

--- filesystem/src/filesystem.c
+++ filesystem/src/filesystem.c
@@ -1,30 +1,35 @@
 #include <filesystem.h>
 
-
 int main(int argc, char* argv[]) {
 
     inicializar_FS();
-
+    fd_FS = iniciar_servidor (valores_config_FS->puerto_escucha, FS_logger, "Servidor FS iniciado");
+    inicializar_bitmap();
+    inicializar_bloques();
+    
     conectar_memoria();
 
-    //finalizar_conexiones(1, cliente_memoria);
-    //finalizar_modulo(logger_FS, valores_config_FS->config);
-    log_destroy(FS_logger);
+    //liberar los logs y config
+    finalizar_modulo(FS_logger, FS_logs_obligatorios, valores_config_FS->config);
     free(valores_config_FS);
-    
+
+    //finalizar las conexiones
+    close(fd_memoria);
+    close(fd_FS);
     return EXIT_SUCCESS;
 }
 
 void inicializar_bitmap(){
-    
-    uint32_t bitmap_size=(BLOCK_COUNT )/8;//que seria block_count?
-    bitmap_size+=7;//redondeo para arriba
+
+    int bitmap_size=(valores_config_FS->block_count +7 )/8;
+ 
+ 
     //es asi? o coomo con bloques? no me cuadra lo del filepath
-    char file_path[100];
-    sprintf(file_path,sizeof(file_path),%s/bitmap.dat,MOUNT_DIR);//que seria mount dir?
+    char bitmap_path[100];
+    sprintf(bitmap_path,"%s/bitmap.dat",valores_config_FS->mount_dir);
 
 
-   FILE* file_bitmap=fopen(file_path,"wd");
+   FILE* file_bitmap=fopen(bitmap_path,"wb");//Probar lo de b o d  ""rb+""
    if(!file_bitmap){
     log_error(FS_logger,"Erorr al crear el archivo bitmap");
     exit (EXIT_FAILURE);
@@ -35,23 +40,50 @@ void inicializar_bitmap(){
     {
         fwrite(&byte_vacio,sizeof(unsigned char),1,file_bitmap);
     }
+    
     fclose(file_bitmap);
-    log_info(FS_logger,"Bitmap inicializado")
-   
+    log_info(FS_logger,"Bitmap inicializado correctamente");
+    log_info(FS_logs_obligatorios,"Creacion Archivo: ## Archivo Creado: <%s> - tamanio: <%d> ", bitmap_path, bitmap_size );
 }
 void inicializar_bloques(){
 
-    FILE* file_bloques=fopen("bloques.dat","wd");
+    char bloques_path[100];
+    sprintf(bloques_path,"%s/bloques.dat",valores_config_FS->mount_dir);
+    FILE* file_bloques=fopen(bloques_path,"wb");
     
     if (!file_bloques)
     {
         log_error(FS_logger,"Erorr al crear el archivo");
         exit (EXIT_FAILURE);
     }
-    fseek(file_bloques,BLOCK_COUNT*BLOCK_SIZE-1,SEKK_SET);
+
+    fseek(file_bloques,valores_config_FS->block_count*valores_config_FS->block_size-1,SEEK_SET);
+    
     fputc('\0',file_bloques);
 
     fclose(file_bloques);
-    log_info(FS_logger,"Archivo de bloques inicializado");
+    log_info(FS_logger,"Archivo de bloques inicializado correctamente");
+    
+
+
+}
+void inicializar_metadata(uint32_t size, uint32_t index_block,char* nombre_archivo){
+
+
+    char metadata_path[1024];
+    sprintf(metadata_path,"%s/files/%s",valores_config_FS->mount_dir,nombre_archivo);
+
+    FILE* metadata_file=fopen(metadata_path,"wb");
+    if (!metadata_file)
+    {
+        log_error(FS_logger,"Erorr al crear el archivo");
+        exit (EXIT_FAILURE);
+    }
+
+    fprintf(metadata_file,"SIZE=%d",size);
+    fprintf(metadata_file,"INDEX_BLOCK=%d",index_block);
+    
+    fclose(metadata_file);
+    log_info(FS_logger,"Archivo metadata inicializado correctamente");
 
 }
